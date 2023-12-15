e3402fd593232935e620600a74ffa508fee77d50
pestana1984 <pestana1984@gmail.com>
2023-12-14 22:51:58 -0300

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o título da categoria!")]
+        [MinLength(3, ErrorMessage = "O título deve ter mais de 3 letras")]
+        [MaxLength(60, ErrorMessage = "O título deve ter no máximo 60 letras")]
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [MinLength(3, ErrorMessage = "O título deve ter mais de 3 letras")]
+        [MaxLength(120, ErrorMessage = "O título deve ter no máximo 120 letras")]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O preço do produto deve ser informado!")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
